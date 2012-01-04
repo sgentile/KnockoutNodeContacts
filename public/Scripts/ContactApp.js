3a5fe02610e311e17d465825afefec8080a29db9
sgentile <steven.gentile@gmail.com>
2012-01-04 13:06:17 -0500

    applied first list

--- public/Scripts/ContactApp.js
+++ public/Scripts/ContactApp.js
@@ -1,28 +1,10 @@
-//function ContactsViewModel(){
-//	this.contactsArray = ko.observableArray([
-//		{firstname:'Steve', lastname: 'Gentile'}
-//	]);
-//}
-
-//ko.applyBindings(new ContactsViewModel(), document.getElementById('contactsRegion'));
-//ko.applyBindings(new ContactsViewModel());
-
-function AppViewModel() {
-    var self = this;
-     
-    self.people = ko.observableArray([
-        { name: 'Bert' },
-        { name: 'Charles' },
-        { name: 'Denise' }
-    ]);
-     
-    self.addPerson = function() {
-        self.people.push({ name: "New at " + new Date() });
-    };
-     
-    self.removePerson = function() {
-        self.people.remove(this);
-    }
+function ContactsViewModel(){
+	var self = this;
+	self.contactsArray = ko.observableArray([
+		{firstname:'Steve', lastname: 'Gentile'}
+	]);
 }
- 
-ko.applyBindings(new AppViewModel());
+$(function(){
+	ko.applyBindings(new ContactsViewModel());
+});
+
