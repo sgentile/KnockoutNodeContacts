550a909a778d3fbf69b66947026386460d692ca4
sgentile <steven.gentile@gmail.com>
2012-01-04 13:35:36 -0500

    fixed spec

--- specs/contactlist_spec.js
+++ specs/contactlist_spec.js
@@ -1,19 +1,28 @@
-require('jasmine-node');
-var soda = require('soda'), 
-	assert = require('assert');
+var soda = require('soda');
 
 var browser = null;
-
 describe('Contact list feature', function(){
+	
+	beforeEach(function(){
+	browser = soda.createClient({
+		host: 'localhost',
+		port: 4444,
+		url: 'http://localhost:3000',
+		browser: 'firefox'
+	});
+});
+
 	it('should exist', function(){
 		browser
-			.chain()
-			.open('/Contact')
-			.waitForElementPresent('id=contactsList', 2000)			
-			.testComplete()
-			.end(function(err){
-				if(err) throw err;
-				console.log('done');
-			)};	
-	);
+		.chain
+		.session()
+		.open('/')
+		//.waitForElementPresent('id=contactsList', 2000)			
+		.testComplete()
+		.end(function(err){
+			if(err) throw err;
+			console.log('done');
+		});
+	});	
 });
+
