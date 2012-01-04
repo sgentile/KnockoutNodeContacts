0dc598318d4fd99479ddd3e6c4d99f06aed07357
sgentile <steven.gentile@gmail.com>
2012-01-04 15:57:19 -0500

    added test to retrieve and validate contact list

--- public/Scripts/ContactApp.js
+++ public/Scripts/ContactApp.js
@@ -1,10 +1,23 @@
+function Contact(data){
+	var self = this;
+	self.firstname = ko.observable(data.firstname);
+	self.lastname = ko.observable(data.lastname);
+	self.fullname = ko.computed(function(){
+		return self.firstname() + " " + self.lastname();
+	});
+}
+
 function ContactsViewModel(){
 	var self = this;
-	self.contactsArray = ko.observableArray([
-		{firstname:'Steve', lastname: 'Gentile'}
-	]);
+	self.contactsArray = ko.observableArray([]);
+	$.getJSON("/Contact", function(data){
+		var mappedContacts = $.map(data, function(item){
+			return new Contact(item);
+		});
+		self.contactsArray(mappedContacts);
+	});
 }
 $(function(){
-	ko.applyBindings(new ContactsViewModel());
+	ko.applyBindings(new ContactsViewModel(), document.getElementById('contactsRegion'));
 });
 
--- specs/contactlist_spec.js
+++ specs/contactlist_spec.js
@@ -9,20 +9,35 @@ describe('Contact list feature', function(){
 		port: 4444,
 		url: 'http://localhost:3000',
 		browser: 'firefox'
+		});
 	});
-});
 
 	it('should exist', function(){
 		browser
 		.chain
 		.session()
 		.open('/')
-		.assertElementPresent('id=contactsList', 2000)			
+		.waitForPageToLoad(3000)
+		.assertElementPresent('id=contactsList')			
 		.testComplete()
 		.end(function(err){
 			if(err) throw err;
 			console.log('done');
 		});
-	});	
+	});
+
+	it("should contain record with full name 'Steve Gentile'", function(){
+		browser
+			.chain
+			.session()
+			.open('/')
+			.waitForPageToLoad(3000)
+			.assertElementPresent('css=#contactsList span:contains(Steve Gentile)')
+			.testComplete()
+			.end(function(err){
+				if(err) throw err;
+				console.log('done');
+			});
+	}); 	
 });
 
